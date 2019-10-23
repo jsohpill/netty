3ccd246ff1c898408a72c9aed39f18eafc499760
chuandadexiaoyu <yuyanlin201268@gmail.com>
2019-10-23 15:55:06 +0800

    EchoClient channel active时候，不向服务器发送数据

--- example/src/main/java/io/netty/example/echo/EchoClientHandler.java
+++ example/src/main/java/io/netty/example/echo/EchoClientHandler.java
@@ -41,7 +41,7 @@ public class EchoClientHandler implements ChannelInboundHandler {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(firstMessage);
+//        ctx.writeAndFlush(firstMessage);
     }
 
     @Override
